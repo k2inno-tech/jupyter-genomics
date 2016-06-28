0b93a6d5e000568c15a9aecb0c4641a3c0ff0c85
Amanda Birmingham <abirmingham@ucsd.edu>
2016-06-28 14:43:35 -0700

    Modify pipeline script to match expected folder/etc structure on AWS AMI

--- src/crispr/mali_pipeliner.py
+++ src/crispr/mali_pipeliner.py
@@ -22,8 +22,8 @@ class PipelineSteps(enum.Enum):
 def main():
     # *********************************************************
     # you'll probably change these on every run, unless you're reprocessing an existing dataset for some reason
-    fastq_set_name = "20160504_D00611_0275_AHMM2JBCXX"
-    human_readable_name = "20160504_Hela_MV4"
+    fastq_set_name = "20160627_HeLa_A549_CV4"
+    human_readable_name = "20160627_HeLa_A549_CV4"
 
     # *********************************************************
     # you'll probably change these lines only when changing the construct library from which you're analyzing data.
@@ -31,18 +31,18 @@ def main():
     # and that the first line will be skipped (as a header row).
 
     # option 1: info for CV4
-    # spacers_file_name = "CV4_2spacers.txt"  # for CV4
-    # full_5p_r1 = "TATATATCTTGTGGAAAGGACGAAACACCG"
-    # full_5p_r2 = "CCTTATTTTAACTTGCTATTTCTAGCTCTAAAAC"
-    # full_3p_r1 = "GTTTCAGAGCTATGCTGGAAACTGC"
-    # full_3p_r2 = "CAAACAAGGCTTTTCTCCAAGG"
-
-    # option 2: info for MV4
-    spacers_file_name = "Metabolism_dual_spacers.txt"
+    spacers_file_name = "CV4_2spacers.txt"  # for CV4
     full_5p_r1 = "TATATATCTTGTGGAAAGGACGAAACACCG"
     full_5p_r2 = "CCTTATTTTAACTTGCTATTTCTAGCTCTAAAAC"
-    full_3p_r1 = "GTTTCAGAGCTATGCTGGAAACTGCATAGCAAGTTGAAATAAGGCTAGTCCGTTATCAACTTGAAAAAGTGGCACCGAGTCGGTGCTTTTTTGTACTGAG"
-    full_3p_r2 = "CAAACAAGGCTTTTCTCCAAGGGATATTTATAGTCTCAAAACACACAATTACTTTACAGTTAGGGTGAGTTTCCTTTTGTGCTGTTTTTTAAAATA"
+    full_3p_r1 = "GTTTCAGAGCTATGCTGGAAACTGC"
+    full_3p_r2 = "CAAACAAGGCTTTTCTCCAAGG"
+
+    # option 2: info for MV4
+    # spacers_file_name = "Metabolism_dual_spacers.txt"
+    # full_5p_r1 = "TATATATCTTGTGGAAAGGACGAAACACCG"
+    # full_5p_r2 = "CCTTATTTTAACTTGCTATTTCTAGCTCTAAAAC"
+    # full_3p_r1 = "GTTTCAGAGCTATGCTGGAAACTGCATAGCAAGTTGAAATAAGGCTAGTCCGTTATCAACTTGAAAAAGTGGCACCGAGTCGGTGCTTTTTTGTACTGAG"
+    # full_3p_r2 = "CAAACAAGGCTTTTCTCCAAGGGATATTTATAGTCTCAAAACACACAATTACTTTACAGTTAGGGTGAGTTTCCTTTTGTGCTGTTTTTTAAAATA"
 
     # *********************************************************
     # you'll probably decide on these values before beginning screen analysis, set them once, and then leave them alone
@@ -50,14 +50,14 @@ def main():
     len_of_seq_to_match = 19
     num_allowed_mismatches = 1
     num_processors = 3
-    main_dir = "/Users/Birmingham/Repositories/ccbb_tickets/20160210_mali_crispr"
+    main_dir = "/home/ec2-user/jupyter-genomics"
 
     # *********************************************************
     # you'll probably never change these, assuming you use the suggested file structure
-    notebooks_dir = os.path.join(main_dir, "notebooks")
-    data_dir = os.path.join(main_dir, "data")
+    notebooks_dir = os.path.join(main_dir, "notebooks/crispr")
+    data_dir = "/data"
     raw_dir = os.path.join(data_dir, "raw")
-    raw_general_dir = os.path.join(raw_dir, "general")
+    library_def_dir = os.path.join(main_dir, "library_definitions")
     interim_fastq_set_dir = os.path.join(data_dir, "interim", fastq_set_name)
     processed_dir = os.path.join(data_dir, "processed")
 
@@ -70,7 +70,7 @@ def main():
                     PipelineSteps.COUNT_COMBINATION,
                     PipelineSteps.COUNT_PLOTTING]
 
-    shared_params = {'g_code_location': os.path.join(main_dir, "src/python"),
+    shared_params = {'g_code_location': os.path.join(main_dir, "src/crispr"),
                      'g_timestamp': "{timestamp}",
                      DATASET_NAME_KEY: human_readable_name,
                      'g_num_processors': num_processors,
@@ -84,7 +84,7 @@ def main():
                      ALG_NAME_KEY: "{0}mer_{1}mm_py".format(len_of_seq_to_match, num_allowed_mismatches),
                      'g_len_of_seq_to_match': len_of_seq_to_match,
                      'g_num_allowed_mismatches': num_allowed_mismatches,
-                     'g_constructs_fp': os.path.join(raw_general_dir, spacers_file_name),
+                     'g_constructs_fp': os.path.join(library_def_dir, spacers_file_name),
                      'g_fastq_counts_dir': interim_fastq_set_dir,
                      'g_fastq_counts_run_prefix': human_readable_name + "_{timestamp}",
                      'g_collapsed_counts_dir': "{run_dir}",
